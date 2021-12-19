ad87ddefc23f3b9702effd39c78b06feaa43bf79
Jinny <jinny.choi07@gmail.com>
2021-12-19 22:10:13 +0900

    backgroundSize js

---
+++ static/js/backgroundSize.js
@@ -0,0 +1,10 @@
+var body = document.body,
+  html = document.documentElement;
+var heightimg = Math.max(
+  body.scrollHeight,
+  body.offsetHeight,
+  html.clientHeight,
+  html.scrollHeight,
+  html.offsetHeight
+);
+document.getElementById("bgimageid").style.height = `${heightimg}px`;
