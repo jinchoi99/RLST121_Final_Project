0703dfb9f4e91f4af635d57dcec41668bdaba872
Jinny <jinny.choi07@gmail.com>
2021-12-19 21:49:45 +0900

    set up pages

--- app.py
+++ app.py
@@ -3,7 +3,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+  return render_template("home.html")
+
+@app.route("/survey")
+def survey():
+  return render_template("survey.html")
+
+@app.route("/summary")
+def summary():
+  return render_template("summary.html")
+
+@app.route("/mbti")
+def mbti():
+  return render_template("mbti.html")
+
+@app.route("/facereader")
+def facereader():
+  return render_template("facereader.html")
+
+@app.route("/sajupalja")
+def sajupalja():
+  return render_template("sajupalja.html")
 
 if __name__ == "__main__":
   app.run()
