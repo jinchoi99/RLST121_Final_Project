5156341624141b4cc5964162d9c46ea97f522e4f
Jinny <jinny.choi07@gmail.com>
2021-12-20 06:18:26 +0900

    debug

--- unse.py
+++ unse.py
@@ -5,40 +5,48 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
-from selenium.webdriver.common.touch_actions import TouchActions
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
 import time
+import os
+from selenium.webdriver.support.wait import WebDriverWait 
+import selenium.webdriver.support.expected_conditions as EC
 
 def all():
     url = "https://www.unsin.co.kr/unse/saju/total/form"
-    driver_loc = "/Users/jchoi/Desktop/crawling/chromedriver"
-    s = Service(driver_loc)
-    chrome_options = Options()
-    # headless, run without opening browser
-    chrome_options.add_argument("headless")
-    chrome_options.add_experimental_option("detach", True)
-    driver = webdriver.Chrome(service=s, options=chrome_options)
+    
+    gChromeOptions = webdriver.ChromeOptions()
+    gChromeOptions.add_argument("window-size=1920x1480")
+    gChromeOptions.add_argument("disable-dev-shm-usage")
+    gChromeOptions.add_argument("--headless")
+    gChromeOptions.add_argument("--disable-dev-shm-usage")
+    gChromeOptions.add_argument("--no-sandbox")
+    gChromeOptions.add_argument("headless")
+    gChromeOptions.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(
+        chrome_options=gChromeOptions, executable_path=ChromeDriverManager().install()
+    )
 
     driver.get(url)
-    usrname = driver.find_element(By.XPATH, "//*[@id='user_name']")
-    usrname.send_keys("허문이")
-    year_menu = driver.find_element(By.XPATH, "//*[@id='birth_yyyy']")
-    select = Select(year_menu)
-    select.select_by_value('2000')
-    month_menu = driver.find_element(By.XPATH, "//*[@id='birth_mm']")
-    select = Select(month_menu)
-    select.select_by_value('08')
-    day_menu = driver.find_element(By.XPATH, "//*[@id='birth_dd']")
-    select = Select(day_menu)
-    select.select_by_value('07')
+    # usrname = driver.find_element(By.XPATH, "//*[@id='user_name']")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user_name']"))).send_keys("허문이")
+    # usrname.send_keys("허문이")
+    # year_menu = driver.find_element(By.XPATH, "//*[@id='birth_yyyy']")
+    # select = Select(year_menu)
+    # select.select_by_value('2000')
+    # month_menu = driver.find_element(By.XPATH, "//*[@id='birth_mm']")
+    # select = Select(month_menu)
+    # select.select_by_value('08')
+    # day_menu = driver.find_element(By.XPATH, "//*[@id='birth_dd']")
+    # select = Select(day_menu)
+    # select.select_by_value('07')
     actions = ActionChains(driver)
     actions.send_keys(Keys.ENTER)
     actions.perform()
-    time.sleep(3)
+    time.sleep(10)
 
     html = driver.page_source
-    # print(html)
     soup = BeautifulSoup(html, 'html.parser')
     r = soup.find(class_='one_m')
     gender = r.find(class_='gender')
@@ -46,8 +54,6 @@ def all():
     dd = r.findAll("dd")
     birth = r.find_all("td")
     content = soup.find(class_="content").find(class_="cont").find("dd").get_text()
-    # print(birth)
-    # title_html = soup.find(class_='group_service NM_FAVORITE_ALL_LY').find_all(class_='list_service')[4].select(".service_data")
     res=""
     for i in birth:
         res += i.find("p").get_text()
@@ -55,8 +61,5 @@ def all():
         res += i.find("img").attrs["src"]
         res += "\n"
     res += content.strip()
-
-    # with open("imgresult.txt", "w", encoding="utf8") as f:
-    #     f.write(res)
     driver.quit()
     return res
