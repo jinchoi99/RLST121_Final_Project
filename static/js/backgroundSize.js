3b7199a85423a1a82b4f9413b8f9f5d9819f9307
Jinny <jinny.choi07@gmail.com>
2021-12-19 22:14:20 +0900

    js position

--- static/js/backgroundSize.js
+++ static/js/backgroundSize.js
@@ -1,10 +1,12 @@
-var body = document.body,
-  html = document.documentElement;
-var heightimg = Math.max(
-  body.scrollHeight,
-  body.offsetHeight,
-  html.clientHeight,
-  html.scrollHeight,
-  html.offsetHeight
-);
-document.getElementById("bgimageid").style.height = `${heightimg}px`;
+window.onload = function() {
+  var body = document.body,
+    html = document.documentElement;
+  var heightimg = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  );
+  document.getElementById("bgimageid").style.height = `${heightimg}px`;
+};
