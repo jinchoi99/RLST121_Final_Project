39cd6e3f800f5aa4c3eed97616ed6b378785f445
Jinny <jinny.choi07@gmail.com>
2021-12-20 15:05:39 +0900

    sdf

--- unse.py
+++ unse.py
@@ -9,21 +9,22 @@ import time
 import os
 from selenium.webdriver.support.wait import WebDriverWait 
 import selenium.webdriver.support.expected_conditions as EC
+from pyvirtualdisplay import Display
 
 def all(name, sex, year, month, day, hour):
-    url = "https://www.unsin.co.kr/unse/saju/total/form"
+    display = Display(visible=False, size=(800, 600))
+    display.start()
+    
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox')
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    url = "https://www.unsin.co.kr/unse/saju/total/form"
+    
     driver.get(url)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user_name']"))).send_keys("Jinny")
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[3]/span[1]"))).click()
-    
-    
-    
+    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user_name']"))).send_keys("Jinny")
+    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[3]/span[1]"))).click()
     # ddelement= Select(WebDriverWait(driver, 10).until(EC.element_to_be_selected((By.ID, "birth_yyyy"))))
     # ddelement.select_by_index(1)
     # driver.find_element_by_css_selector("#birth_yyyy [value=\"1999\"]").click()
@@ -41,5 +42,6 @@ def all(name, sex, year, month, day, hour):
     # print(tds)
     
     # img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
-    driver.quit()
+    # driver.quit()
+    display.stop()
     return [html2, "img1", "imgtxt1"]
