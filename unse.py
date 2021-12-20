7e0b5bfa8ec4c422b5d8a7ae439272895ff43934
Jinny <jinny.choi07@gmail.com>
2021-12-20 11:06:03 +0900

    xpath fix

--- unse.py
+++ unse.py
@@ -13,7 +13,6 @@ import os
 from selenium.webdriver.support.wait import WebDriverWait 
 import selenium.webdriver.support.expected_conditions as EC
 
-import traceback
 
 def all(name, sex, year, month, day, hour):
     url = "https://www.unsin.co.kr/unse/saju/total/form"
@@ -54,14 +53,11 @@ def all(name, sex, year, month, day, hour):
     actions.perform()
     time.sleep(10)
     
-    try:
-        context = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div/div[3]/dl[1]/dd"))).text
-        
-        imgtxt1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/p"))).text
-        
-        img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
+    # context = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div/div[3]/dl[1]/dd"))).text
     
-    except:
-        traceback.print_exc()
+    imgtxt1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/p"))).text
     
-    return [context, img1, imgtxt1]
+    img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
+    
+    
+    return ["context", img1, imgtxt1]
