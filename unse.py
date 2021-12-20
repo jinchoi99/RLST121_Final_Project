388ad24b23468e1bb8830bd8db867028d515e40e
Jinny <jinny.choi07@gmail.com>
2021-12-20 12:16:43 +0900

    wow

--- unse.py
+++ unse.py
@@ -21,23 +21,23 @@ def all(name, sex, year, month, day, hour):
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     driver.get(url)
     WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='user_name']"))).send_keys(str(name))
-    year_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_yyyy']")))
-    select = Select(year_menu)
-    if sex == "Male":
-        sex_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[2]/dd/span[1]/label")))
-    else:
-        sex_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[2]/dd/span[2]/label")))
-    sex_menu.click()
-    select.select_by_value(str(year))
-    month_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_mm']")))
-    select = Select(month_menu)
-    select.select_by_value(str(month))
-    day_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_dd']")))
-    select = Select(day_menu)
-    select.select_by_value(str(day))
-    hour_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_hh']")))
-    select = Select(hour_menu)
-    select.select_by_value(str(hour))
+    # year_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_yyyy']")))
+    # select = Select(year_menu)
+    # if sex == "Male":
+    #     sex_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[2]/dd/span[1]/label")))
+    # else:
+    #     sex_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='frm']/div[1]/dl[2]/dd/span[2]/label")))
+    # sex_menu.click()
+    # select.select_by_value(str(year))
+    # month_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_mm']")))
+    # select = Select(month_menu)
+    # select.select_by_value(str(month))
+    # day_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_dd']")))
+    # select = Select(day_menu)
+    # select.select_by_value(str(day))
+    # hour_menu = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='birth_hh']")))
+    # select = Select(hour_menu)
+    # select.select_by_value(str(hour))
     actions = ActionChains(driver)
     actions.send_keys(Keys.ENTER)
     actions.perform()
@@ -49,6 +49,5 @@ def all(name, sex, year, month, day, hour):
     # print(tds)
     
     # img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
-    
     
     return [html, "img1", "imgtxt1"]
