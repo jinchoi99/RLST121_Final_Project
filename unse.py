4506b2cc9badf076697c32d3537582e5c704b893
Jinny <jinny.choi07@gmail.com>
2021-12-20 12:35:59 +0900

    try display

--- unse.py
+++ unse.py
@@ -8,13 +8,15 @@ import time
 import os
 from selenium.webdriver.support.wait import WebDriverWait 
 import selenium.webdriver.support.expected_conditions as EC
+from pyvirtualdisplay import Display
 
 def all(name, sex, year, month, day, hour):
     url = "https://www.unsin.co.kr/unse/saju/total/form"
-    
+    display = Display(visible=False, size=(800, 600))
+    display.start()
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
@@ -48,5 +50,6 @@ def all(name, sex, year, month, day, hour):
     # print(tds)
     
     # img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
-    
+    display.stop()
+    # driver.quit()
     return [html, "img1", "imgtxt1"]
