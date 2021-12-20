f6a6835bd5c05933ec25d7d9f0ca1212b815aded
Jinny <jinny.choi07@gmail.com>
2021-12-20 11:54:52 +0900

    sdfdsf

--- app.py
+++ app.py
@@ -29,17 +29,14 @@ def sajupalja():
 
 @app.route("/calc", methods=['GET', 'POST'])
 def calc():
-  if request.method == 'POST':
-    name = str(request.form['name_input'])
-    sex = str(request.form['sex_input'])
-    year = str(request.form['year_input'])
-    month = str(request.form['month_input'])
-    day = str(request.form['day_input'])
-    hour = str(request.form['hour_input'])
-    [exp, img, imgtxt] = unse.all(name, sex, year, month, day, hour)
-    return render_template("sajupalja-result.html", rName=name, rYear=year, rMonth=month, rDay=day, rHour=hour, rSex=sex, rExp = exp, rImg = img, rTxt = imgtxt)
-  else:
-    return render_template("sajupalja.html")
+  name = str(request.form['name_input'])
+  sex = str(request.form['sex_input'])
+  year = str(request.form['year_input'])
+  month = str(request.form['month_input'])
+  day = str(request.form['day_input'])
+  hour = str(request.form['hour_input'])
+  [exp, img, imgtxt] = unse.all(name, sex, year, month, day, hour)
+  return render_template("sajupalja-result.html", rName=name, rYear=year, rMonth=month, rDay=day, rHour=hour, rSex=sex, rExp = exp, rImg = img, rTxt = imgtxt)
 
 if __name__ == "__main__":
   app.run()
