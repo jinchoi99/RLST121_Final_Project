72df0ea51320fa5e04a6411e1b5788a8f357887f
Jinny <jinny.choi07@gmail.com>
2021-12-20 13:31:31 +0900

    wow

--- unse.py
+++ unse.py
@@ -23,19 +23,15 @@ def all(name, sex, year, month, day, hour):
     
     html = driver.page_source
     soup = BeautifulSoup(html, 'html.parser')
-    r = soup.find(id="birth_yyyy").find_all("option")
+    yrs = soup.find(id="birth_yyyy").find_all("option")
+    for y in yrs:
+        y['value'] = '1999'
     
-    # for i in birth:
-    #     res += i.find("p").get_text()
-    #     res += ": "
-    #     res += i.find("img").attrs["src"]
-    #     res += "\n
-    
-    # actions = ActionChains(driver)
-    # actions.send_keys(Keys.ENTER)
-    # actions.perform()
-    # time.sleep(10)
-    # html = driver.page_source
+    actions = ActionChains(driver)
+    actions.send_keys(Keys.ENTER)
+    actions.perform()
+    time.sleep(10)
+    html2 = driver.page_source
     # context = driver.find_element(By.XPATH, "/html/body/div[2]/div/div[2]/div/div/div/div/div[3]/dl[1]/dd").text
     # imgtxt1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID,"sjms_side")))
     # tds = WebDriverWait(imgtxt1, 10).until(EC.element_to_be_clickable((By.TAG_NAME,"td")))
@@ -43,4 +39,4 @@ def all(name, sex, year, month, day, hour):
     
     # img1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='sjms_side']/table/tbody/tr[1]/td[2]/img"))).get_attribute("src")
     driver.quit()
-    return [str(r), "img1", "imgtxt1"]
+    return [html2, "img1", "imgtxt1"]
