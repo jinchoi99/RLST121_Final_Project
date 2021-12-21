345ab7f79e92ad2b4037df89c2d1f4398bd0bf94
Jinny <jinny.choi07@gmail.com>
2021-12-21 09:32:49 +0900

    done impl saju test

--- app.py
+++ app.py
@@ -3,11 +3,6 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# @app.route("/testp")
-# def testpp():
-#   hi = testp.all()
-#   return render_template("index.html", rhi=hi)
-
 @app.route("/")
 def home():
   return render_template("home.html")
@@ -40,10 +35,7 @@ def calc():
   month = str(request.form['month_input'])
   day = str(request.form['day_input'])
   hour = str(request.form['hour_input'])
-  exp = "explanation"
-  img = "image.jpg"
-  imgtxt = "imgtxt"
-  return render_template("sajupalja-result.html", rName=name, rYear=year, rMonth=month, rDay=day, rHour=hour, rSex=sex, rExp = exp, rImg = img, rTxt = imgtxt)
+  return render_template("sajupalja-result.html", rName=name, rYear=year, rMonth=month, rDay=day, rHour=hour, rSex=sex)
 
 if __name__ == "__main__":
   app.run()
--- static/js/sajuResult.js
+++ static/js/sajuResult.js
@@ -1,14 +1,38 @@
-let sData = [];
-d3.csv('/static/data/2000.csv')
-  .then((data) => {
-    sData = data;
-    console.log(sData)
-  });
+let data2000 = [];
+function start(date) {
+  d3.csv('/static/data/2000.csv')
+    .then((data) => {
+      data2000 = data;
+      console.log(data2000)
+      fillResults(date)
+    });
+}
+
 
 const results = [];
 function fillResults(date) {
-  // for (let i = 0; i < sData.length; i += 1) {
-  //   results.push(sData[i][0]);
-  // }
-  alert("date")
+  for (let i = 0; i < data2000.length; i += 1) {
+    results.push(data2000[i][" "+date]);
+  }
+
+  // console.log(date)
+  // console.log(results[16])
+  
+  document.getElementById("char1").innerHTML = results[0];
+  document.getElementById("char2").innerHTML = results[1];
+  document.getElementById("char3").innerHTML = results[2];
+  document.getElementById("char4").innerHTML = results[3];
+  document.getElementById("char5").innerHTML = results[4];
+  document.getElementById("char6").innerHTML = results[5];
+  document.getElementById("char7").innerHTML = results[6];
+  document.getElementById("char8").innerHTML = results[7];
+  $("#char1img").attr("src",results[8]);
+  $("#char2img").attr("src",results[9]);
+  $("#char3img").attr("src",results[10]);
+  $("#char4img").attr("src",results[11]);
+  $("#char5img").attr("src",results[12]);
+  $("#char6img").attr("src",results[13]);
+  $("#char7img").attr("src",results[14]);
+  $("#char8img").attr("src",results[15]);
+  document.getElementById("saju_exp").innerHTML = results[16];
 }
